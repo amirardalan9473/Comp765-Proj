b461de1d425586fbfb1e1f5fd422dfaa842f337a
amirardalan9473 <amirardalan9473@gmail.com>
2019-04-16 23:35:44 -0400

    wtf am i doin

--- PILCO-master/examples/encoautoencod.py
+++ PILCO-master/examples/encoautoencod.py
@@ -1,39 +1,89 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from keras.layers import Input, Dense
 from keras.models import Model
 
+from keras.layers import Lambda, Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+from keras.losses import mse, binary_crossentropy
+from keras.utils import plot_model
+from keras import backend as K
 
-def autoencod(x_train, x_test, ):
-    encoding_dim = 12  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-    input_img = Input(shape=(4,))
-    encoded = Dense(encoding_dim, activation='relu')(input_img)
-    decoded = Dense(4, activation='sigmoid')(encoded)
-
-    autoencoder = Model(input_img, decoded)
-
-    encoder = Model(input_img, encoded)
-
-    encoded_input = Input(shape=(encoding_dim,))
-
-    decoder_layer = autoencoder.layers[-1]
-
-    decoder = Model(encoded_input, decoder_layer(encoded_input))
-
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-
-    autoencoder.fit(x_train, x_train,
-                    epochs=50,
-                    batch_size=256,
-                    shuffle=True,
-                    validation_data=(x_test, x_test))
-
-    # encoded_imgs = encoder.predict(x_test)
-    # decoded_imgs = decoder.predict(encoded_imgs)
-    decoder.save('decoder.h5')
-    encoder.save('encoder.h5')
-    print("Saved model to disk")
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
 
 
-if __name__ == "__main__":
-    from pend_exp import sampler
+x = Input(batch_shape=(batch_size, original_dim))
+h = Dense(intermediate_dim, activation='relu')(x)
+z_mean = Dense(latent_dim)(h)
+z_log_sigma = Dense(latent_dim)(h)
 
+
+
+
+def sampling(args):
+    z_mean, z_log_sigma = args
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(z_log_sigma) * epsilon
+
+# note that "output_shape" isn't necessary with the TensorFlow backend
+# so you could write `Lambda(sampling)([z_mean, z_log_sigma])`
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_sigma])
+
+
+
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder_mean = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded_mean = decoder_mean(h_decoded)
+
+
+
+vae = Model(x, x_decoded_mean)
+
+# encoder, from inputs to latent space
+encoder = Model(x, z_mean)
+
+# generator, from latent space to reconstructed inputs
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
+
+
+def vae_loss(x, x_decoded_mean):
+    xent_loss = objectives.binary_crossentropy(x, x_decoded_mean)
+    kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
+    return xent_loss + kl_loss
+
+vae.compile(optimizer='rmsprop', loss=vae_loss)
+
+
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+vae.fit(x_train, x_train,
+        shuffle=True,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, x_test))
+
+
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
+plt.colorbar()
+plt.show()
 
